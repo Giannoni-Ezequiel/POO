28fdd6f72a57d873ad0a271afafbdec7f788a8fd
Ezequiel <egiannoni@frba.utn.edu.ar>
2023-07-18 12:58:25 -0300

    Creando un objeto
    Clase Auto con funciones

---
+++ src/main/java/ejemplopoo/Auto.java
@@ -0,0 +1,23 @@
+package ejemplopoo;
+
+List<> = Patentes.toAdd();
+void ImprimirPatente (List<> Patentes, )
+{
+     for(int i = 0; i < List<i>; i++)   
+        List<i> = "";
+        Console.Write($"Patente 
+                                    N
+                                             °: " + List<i>)
+    
+}
+
+string ElegirColor ()
+public class Auto {
+   
+    //declarando atributos
+    int id;
+    String patente;
+    int cant_puertas;
+    String color;
+    boolean caja_manual;
+}
